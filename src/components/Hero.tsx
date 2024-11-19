7ef0899a2d06db4369f26c01b67031998b95a543
Zain_Darbari <topmafiyagamer@gmail.com>
2024-11-19 14:26:16 +0500

    update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,6 +11,7 @@ const Hero = () => {
     >
       <Navbar />
       <div className='container grid lg:grid-cols-2 h-[calc(100vh-60px)]'>
+      <div className='hidden lg:block'></div>
         <div className='text-[80px] sm:text-[100px] font-bold leading-tight flex justify-center items-center'>
           <div data-aos="zoom-in-up">
             <p>I&apos;m</p>
